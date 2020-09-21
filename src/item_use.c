787b1f99f591e5b97af1424bcdcb25eec2a2cd00
BLourenco <brandon.lourenco@gmail.com>
2020-09-21 17:39:27 -0400

    Removed all text prompts when using TMs/HMs

--- src/item_use.c
+++ src/item_use.c
@@ -785,10 +785,13 @@ void ItemUseOutOfBattle_RareCandy(u8 taskId)
 
 void ItemUseOutOfBattle_TMHM(u8 taskId)
 {
-    if (gSpecialVar_ItemId >= ITEM_HM01_CUT)
-        DisplayItemMessage(taskId, 1, gText_BootedUpHM, BootUpSoundTMHM); // HM
-    else
-        DisplayItemMessage(taskId, 1, gText_BootedUpTM, BootUpSoundTMHM); // TM
+    // if (gSpecialVar_ItemId >= ITEM_HM01_CUT)
+    //     DisplayItemMessage(taskId, 1, gText_BootedUpHM, BootUpSoundTMHM); // HM
+    // else
+    //     DisplayItemMessage(taskId, 1, gText_BootedUpTM, BootUpSoundTMHM); // TM
+
+    PlaySE(SE_PC_LOGIN);
+    UseTMHM(taskId);
 }
 
 static void BootUpSoundTMHM(u8 taskId)
