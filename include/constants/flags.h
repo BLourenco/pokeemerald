17b3b7d7d688c928a784195c72ca742e62cc0da3
BLourenco <brandon.lourenco@gmail.com>
2020-09-25 22:01:49 -0400

    Removed debug flag that's no longer needed

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1381,8 +1381,8 @@
 #define FLAG_IS_CHAMPION                            (SYSTEM_FLAGS + 0x1F) // Seems to be related to linking.
 #define FLAG_NURSE_UNION_ROOM_REMINDER              (SYSTEM_FLAGS + 0x20)
 
-#define FLAG_SYS_TOGGLE_TRAINER_SIGHT               (SYSTEM_FLAGS + 0x21) 
-#define FLAG_SYS_TOGGLE_COLLISION                   (SYSTEM_FLAGS + 0x22)
+#define FLAG_SYS_COLLISIONS_DISABLED                (SYSTEM_FLAGS + 0x21) 
+#define FLAG_UNUSED_0x882                           (SYSTEM_FLAGS + 0x22) // Unused Flag
 #define FLAG_UNUSED_0x883                           (SYSTEM_FLAGS + 0x23) // Unused Flag
 #define FLAG_UNUSED_0x884                           (SYSTEM_FLAGS + 0x24) // Unused Flag
 #define FLAG_UNUSED_0x885                           (SYSTEM_FLAGS + 0x25) // Unused Flag
