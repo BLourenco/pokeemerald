1fb23063b2ad124622fc2edad8853ade625902cc
BLourenco <brandon.lourenco@gmail.com>
2020-10-10 23:36:34 -0400

    Eevee can now evolve into Sylveon by using a Shiny Stone, in addition to its default evolution method

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -328,7 +328,7 @@
 
 #define EVO_ITEM_HOLD        0x001F // Pokemon levels up, holds specified item
 
-#define EVOS_PER_MON 12
+#define EVOS_PER_MON 13
 
 #define NUM_MALE_LINK_FACILITY_CLASSES   8
 #define NUM_FEMALE_LINK_FACILITY_CLASSES 8
